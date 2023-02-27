b75b759fb3d3eb5c93f03bb0b19b8b52b4095a07
alex korotkevich <aleksey.korotkevich.1991@mail.com>
2023-02-27 17:17:56 +0300

    proxy

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,38 @@
-const path = require('path')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        doc: path.resolve(__dirname, './src/doc.js'),
-        home: path.resolve(__dirname, './src/home.js'),
+  entry: {
+    doc: path.resolve(__dirname, "/src/doc.js"),
+    home: path.resolve(__dirname, "./src/home.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./public"),
+    filename: "[name].js",
+  },
+  //   plugins: [
+  //     new HtmlWebpackPlugin({
+  //       template: path.join(__dirname, "src", "template.html"),
+  //       filename: "index.html",
+  //     }),
+  //   ],
+  devServer: {
+    watchFiles: path.join(__dirname, ""),
+    static: [
+      {
+        publicPath: "/",
+        directory: ["public", "pages"],
+      },
+      {
+        publicPath: "/docs/",
+        directory: ["public", "pages"],
+      },
+    ],
+    proxy: {
+      "/collab-backend": {
+        target: "http://localhost:8800",
+      },
     },
-    output: {
-        path: path.resolve(__dirname, './public'),
-        filename: '[name].js',
-    },
-}
+    // open: ["index"],
+  },
+};
